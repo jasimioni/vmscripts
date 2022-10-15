d58968b40820081f068a1b9fed75953d1846c567
Joao Andre Simioni <joao.simioni@canonical.com>
2022-10-15 12:40:00 -0300

    sáb 15 out 2022 12:40:00 -03

--- SyncFiles.sh
+++ SyncFiles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 REMOTEDIR=$(pwd | sed -e 's/.*cases\//\/customers\//')
+REMOTE="sftp://files.support.canonical.com$REMOTEDIR"
 
-lftp -c mirror --parallel sftp://files.support.canonical.com$REMOTEDIR .
+echo "Downloading from $REMOTE"
+
+lftp -c mirror --only-missing --parallel $REMOTE .
